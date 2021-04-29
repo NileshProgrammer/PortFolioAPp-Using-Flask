55b093dc9a8f15dfbf35ffa304fbd288b11a7ab6
Nilesh Rawool <rizla.ganpat@gmail.com>
2021-04-29 21:33:15 +0530

    Chnages to app.py for stagging

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World this app is deployed to Heroku App"
+    return "Hello World this app is deployed to Heroku App with pipeline"
